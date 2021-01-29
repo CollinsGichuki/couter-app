99a36e4f07f1ce2cf639985d0fcf45111e85fdf4
Snilloc <collinsgichuki9@gmail.com>
2021-01-29 11:01:28 +0300

    Added Deleting an Element

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
@@ -7,37 +7,42 @@ class Counter extends Component {
 
   //Arrow functions don't bind this keyword
   handleIncrement = () => {
-     //setState tells React we are updating the state 
-     //and it will figure out which part of the state we are updating and bring the DOM in sync with the virtual sync
-     this.setState({value: this.state.value +1})
-  }
+    //setState tells React we are updating the state
+    //and it will figure out which part of the state we are updating and bring the DOM in sync with the virtual sync
+    this.setState({ value: this.state.value + 1 });
+  };
 
   render() {
-    return(
-    <div>
-      {//Accessing Children properties
-      this.props.children}
-      <span 
-      className={ this.getClassBadges()}>{this.formatCount()}</span>
-      <button 
-      onClick = {this.handleIncrement}
-      className="btn btn-secondary btn-sm">Increment</button>
+    return (
+      <div>
+        <span className={this.getClassBadges()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm btn-m4"
+        >
+          Delete
+        </button>
       </div>
-      );
+    );
   }
 
   getClassBadges() {
     let classes = "badge m-2 badge-";
     classes += this.state.value === 0 ? "warning" : "primary";
-    return classes
+    return classes;
   }
 
   formatCount() {
-    const {value: count} = this.state;
+    const { value: count } = this.state;
     //If count is 0, show the string Zero else, show the number
-    return count === 0 ? 'Zero' : count;
+    return count === 0 ? "Zero" : count;
   }
-
 }
 
 export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,29 +1,49 @@
-import React, { Component } from 'react';
-import Counter from './counter';
+import React, { Component } from "react";
+import Counter from "./counter";
 
 class Counters extends Component {
-    state = { 
-        //Array of Counters
-        //Use the id to uniquely identify the counters
-        counters: [
-            {id:0, value: 0},
-            {id: 1, value: 1},
-            {id: 2, value: 2},
-            {id: 3, value: 3},
-            {id: 4, value: 4}
-        ]
-     }
-    render() { 
-        return (
-            <div> {
-                //Get the individual counter and map it to the Counter Component
-                this.state.counters.map(counter => (
-                    <Counter  key={counter.id} value={counter.value}>,
-                    <h4>Counter {counter.id}</h4>
-                    </Counter>
-                ))}
-        </div> );
-    }
+  state = {
+    //Array of Counters
+    //Use the id to uniquely identify the counters
+    counters: [
+      { id: 0, value: 0 },
+      { id: 1, value: 1 },
+      { id: 2, value: 2 },
+      { id: 3, value: 3 },
+      { id: 4, value: 4 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    //We don't update the state completely
+    //To remove an element from the counters array, we make a new array without the deleted array
+    //call setState on the component and let React do
+    //Use the filter method to get all the counters except the one with the id
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    console.log("CounterId of tag selected", counterId);
+    //Override the counters property with the counters const
+    this.setState({ counters: counters });
+  };
+
+  render() {
+    return (
+      <div>
+        {
+          //Get the individual counter and map it to the Counter Component
+          this.state.counters.map((counter) => (
+            <Counter
+              //Props
+              //Key is used internally by React and hence why we have to create id prop
+              key={counter.id}
+              onDelete={this.handleDelete}
+              value={counter.value}
+              id={counter.id}
+            ></Counter>
+          ))
+        }
+      </div>
+    );
+  }
 }
- 
+
 export default Counters;
